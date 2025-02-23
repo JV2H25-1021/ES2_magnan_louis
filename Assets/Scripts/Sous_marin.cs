41b43c49b0a8191eb8f535ab2967d729053ab937
XGrosMatouX <louismagnan65@gmail.com>
2025-02-23 17:49:38 -0500

    indenter le code

--- Assets/Scripts/Sous_marin.cs
+++ Assets/Scripts/Sous_marin.cs
@@ -5,15 +5,14 @@ using UnityEngine.InputSystem;
 using UnityEngine.Windows;
 public class Sous_marin : MonoBehaviour
 {
-
+    // Variable controle des mouvements
     [SerializeField] private float _vitessePromenade;
 
     private Rigidbody _rb;
     private Vector3 directionInput;
-    
+    // Variable controle des animations
     [SerializeField] private float _modifierAnimTranslation;
     private Animator _animator;
-    private float _rotationVelocity;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,9 +21,11 @@ public class Sous_marin : MonoBehaviour
     }
     void OnPromener(InputValue directionBase) 
     {
+        //deplacement du sous-marin
         Vector2 directionAvecVitesse = directionBase.Get<Vector2>() * _vitessePromenade;
         directionInput = new Vector3(0f, directionAvecVitesse.y, directionAvecVitesse.x);
     }
+    // Press shitf vitesse double
     void OnShiftPressed(InputValue etatBouton)
     {
         if(etatBouton.isPressed)
@@ -40,12 +41,12 @@ public class Sous_marin : MonoBehaviour
     {
         Vector3 mouvement = directionInput;
         _rb.AddForce(mouvement, ForceMode.VelocityChange);
-
+        //Gestion des animation
         Vector3 vitesseSurPlane = new Vector3(0f, _rb.velocity.y, _rb.velocity.z);
-
+        //Animation en Z
         _animator.SetFloat("VitesseZ", vitesseSurPlane.z * _modifierAnimTranslation);
         _animator.SetFloat("DeplacementZ", vitesseSurPlane.z);
-
+        //Animation en Y
         _animator.SetFloat("VitesseY", vitesseSurPlane.y * _modifierAnimTranslation);
         _animator.SetFloat("DeplacementY", vitesseSurPlane.y);
     }
